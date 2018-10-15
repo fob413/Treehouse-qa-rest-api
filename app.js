7dc90e7b8047460ca58da88046927b75a135189f
fob1493 <fob1493@gmail.com>
2018-10-15 13:07:30 +0100

    Add Database integration

--- app.js
+++ app.js
@@ -1,8 +1,61 @@
 'use strict';
 
 var express = require('express');
+var jsonParser = require('body-parser').json;
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var routes = require('./routes');
 
 var app = express();
+
+app.use(logger("dev"));
+app.use(jsonParser());
+
+mongoose.connect('mongodb://localhost:27017/qa');
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.error("connection error:", err);
+});
+
+db.once("open", function() {
+  console.log("db connection successful");
+});
+
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	if(req.method === "OPTIONS") {
+		res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+		return res.status(200).json({});
+	}
+	next();
+});
+
+app.use('/questions', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+app.use(function(req, res, next) {
+  console.log('first piece of middle ware');
+  next();
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
